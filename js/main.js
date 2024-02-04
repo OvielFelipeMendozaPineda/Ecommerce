fc804345d576df78ab260b16b3be1f7b3a1d41a3
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-02-04 15:01:49 -0500

    feat: :constructions: Añadida seccion 'carrito-compras'. Se añadio la seccion de visaulizacion del carrito de compra. Igualmente se añadio estilos y diseño.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 let preview = document.querySelector('.preview');
 let purchaseButtons = document.querySelectorAll('.purchase');
 let close = document.querySelector('.back')
-let nav = document.querySelector('nav')
+let header = document.querySelector('header')
+let carts = document.querySelectorAll('.bx-cart');
+let carrito = document.querySelector('.carrito-compras')
 
 purchaseButtons.forEach(function (button) {
     button.addEventListener('click', function () {
         preview.classList.add('active');
-        nav.classList.add('active');
+        header.classList.add('active');
     });
 });
 
 close.addEventListener('click', function () {
     preview.classList.remove('active');
-    nav.classList.remove('active');
+    header.classList.remove('active');
 })
+
+carts.forEach(function(cart) {
+    cart.addEventListener('click', function() {
+        carrito.classList.toggle('active');
+        header.classList.toggle('active');
+    });
+});
